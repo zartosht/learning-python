df9fa7f79024a7844fd8fe31594cb3f09ab147ff
Zartosht Sepideman <xashayarsha@gmail.com>
2024-02-04 22:12:58 +0300

    Complete lessons

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,12 +23,12 @@ $(document).ready(function () {
     copyButton.className = 'copy-btn';
     copyButton.innerHTML = defaultButtonValue;
 
-    const runButton = document.createElement('button');
-    runButton.className = 'run-btn';
-    runButton.innerHTML = '<i class="bi-play-circle" style="font-size: 1.2rem; color: cornflowerblue;"></i>';
+    // const runButton = document.createElement('button');
+    // runButton.className = 'run-btn';
+    // runButton.innerHTML = '<i class="bi-play-circle" style="font-size: 1.2rem; color: cornflowerblue;"></i>';
 
     buttonsContainer.appendChild(copyButton);
-    buttonsContainer.appendChild(runButton);
+    // buttonsContainer.appendChild(runButton);
 
     const pre = codeBlock.parentNode;
     pre.parentNode.insertBefore(buttonsContainer, pre);
@@ -41,43 +41,45 @@ $(document).ready(function () {
 
     clipboard.on('success', function (e) {
       copyButton.innerHTML = copiedButtonValue;
+      console.log('Copied!');
       e.clearSelection();
     });
 
     copyButton.addEventListener('click', () => {
+      console.log('clicked');
       setTimeout(() => copyButton.innerHTML = defaultButtonValue, 2000);
     });
 
-    runButton.addEventListener('click', () => {
-      const pythonScript = document.createElement('div');
-      pythonScript.className = 'python-script';
-      pythonScript.contentEditable = true;
-      pythonScript.innerHTML = codeBlock.innerText;
+    // runButton.addEventListener('click', () => {
+    //   const pythonScript = document.createElement('div');
+    //   pythonScript.className = 'python-script';
+    //   pythonScript.contentEditable = true;
+    //   pythonScript.innerHTML = codeBlock.innerText;
 
-      const output = document.createElement('div');
-      output.className = 'output';
+    //   const output = document.createElement('div');
+    //   output.className = 'output';
 
-      const runCode = document.createElement('button');
-      runCode.className = 'run-code';
-      runCode.innerHTML = 'Run';
-      runCode.onclick = () => {
-        const output = pythonScript.innerText;
-        const result = brython({
-          debug: 1,
-          stdout: output
-        });
+    //   const runCode = document.createElement('button');
+    //   runCode.className = 'run-code';
+    //   runCode.innerHTML = 'Run';
+    //   runCode.onclick = () => {
+    //     const output = pythonScript.innerText;
+    //     const result = brython({
+    //       debug: 1,
+    //       stdout: output
+    //     });
 
-        console.log(result);
-        output.innerHTML = result;
-      };
+    //     console.log(result);
+    //     output.innerHTML = result;
+    //   };
 
-      codeBlock.parentNode.parentNode.parentNode.appendChild(pythonScript);
-      codeBlock.parentNode.parentNode.parentNode.appendChild(runCode);
-      codeBlock.parentNode.parentNode.parentNode.appendChild(output);
-    });
+    //   codeBlock.parentNode.parentNode.parentNode.appendChild(pythonScript);
+    //   codeBlock.parentNode.parentNode.parentNode.appendChild(runCode);
+    //   codeBlock.parentNode.parentNode.parentNode.appendChild(output);
+    // });
   });
 });
 
-window.addEventListener("load", function () {
-  brython();
-});
+// window.addEventListener("load", function () {
+//   brython();
+// });
