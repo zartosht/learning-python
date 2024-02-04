b8b5af2cc8826b9d832cc6687fe32b301077b457
Zartosht Sepideman <xashayarsha@gmail.com>
2024-02-04 20:40:20 +0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,3 @@
-function copyCodeToClipboard(btn) {
-  const codeBlock = btn.nextElementSibling.querySelector('code');
-  const range = document.createRange();
-  range.selectNode(codeBlock);
-  window.getSelection().removeAllRanges(); // clear current selection
-  window.getSelection().addRange(range); // to select text
-  if (!navigator.clipboard) {
-    document.execCommand('copy');
-  } else {
-    navigator.clipboard.writeText(text_to_copy).then(
-      function () {
-        window.getSelection().removeAllRanges();// to deselect
-        btn.textContent = 'Copied!';
-        setTimeout(function () { btn.textContent = 'Copy'; }, 2000);
-      })
-      .catch(
-        function () {
-          alert("err"); // error
-        });
-  }
-}
-
 $(document).ready(function () {
   const btn = $('#button');
   $(window).scroll(function () {
